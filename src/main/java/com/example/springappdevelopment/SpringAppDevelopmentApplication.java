89d4a588c8e54dc94ed0b8f5ff67d6009990acaf
Rohit <rohitb333@gmail.com>
2022-05-11 19:39:03 +0530

    Welcome to springboot app

---
+++ src/main/java/com/example/springappdevelopment/SpringAppDevelopmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.springappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringAppDevelopmentApplication.class, args);
+    }
+
+}
